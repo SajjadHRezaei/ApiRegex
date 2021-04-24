3aafd1d8b7863d28a6b569e5c0f20ebea6186bf3
Sheeva Ahmadi <sheeva.ahmadi@gmail.com>
2021-04-24 14:13:09 +0000

    wikitable.py edited online with Bitbucket

--- wikitable.py
+++ wikitable.py
@@ -14,3 +14,17 @@ soup = BeautifulSoup(data.text, 'html.parser')
 #find table and table data
 table_1 = soup.find('table', {'class':"wikitable sortable",'id':"constituents"})
 tablebody = table_1.find('tbody')
+ssp=[]
+for i,row in enumerate(tablebody.find_all('tr')):
+    if i==0:
+        th = row.find_all('th')
+        title = [i.text.strip() for i in th]
+    else:
+        td = row.find_all('td')
+        table_row = [i.text.strip() for i in td]
+        ssp.append(table_row)
+ssp=ssp[::-1]
+
+df = pd.DataFrame.from_records(ssp)
+df.columns=title
+df
